8a317e23d598c96a3da0c950c15196b4836430c6
Sherri Ann Gunder <verdu103@mail.chapman.edu>
2015-11-09 17:05:21 +0000

    updatingt test helpers for section 5.3

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,5 +5,7 @@ class UsersControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
     assert_response :success
+    assert_select "title", "Sign up | Zen Blog"
   end
+  
 end
